1afe668e05d4d1baae28e745ad32e9c2931526b9
kylevgriffith <kylevgriffith@gmail.com>
2022-02-07 20:22:45 +0330

    theme setup and gradle config

---
+++ lib/contants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF8065E2);
+const Color accentColor = Color(0xFF37338B);
+const Color primaryTextColor = Color(0xFF232223);
+const Color secondaryTextColor = Color(0xFFFFFFFF);
+const Color backgroundColor = Color(0xFFFDFDFE);
+const Color primaryBackgroundColor = Color(0xFFC3CFE0);
