4fd0c245b0599d01951c601cffe866febe9c167f
kylevgriffith <kylevgriffith@gmail.com>
2022-02-08 19:16:51 +0330

    add DateTime extension

---
+++ lib/helpers/contants.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF8164E3);
+
+const Color accentColor = Color(0xFF37338B);
+const Color thirdColor = Color(0xFFC3CFE0);
+const Color textColor = Color(0xFF232223);
+const Color backgroundColor = Color(0xFFFDFDFE);
+
+const TextStyle kdefaultTextStyle = TextStyle(
+  fontSize: 20,
+  color: textColor,
+);
+const TextStyle kboldTextStyle = TextStyle(
+  fontSize: 20,
+  color: textColor,
+  fontWeight: FontWeight.bold,
+);
+
+const List<String> months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
---
+++ lib/helpers/extesnsion.dart
@@ -0,0 +1,26 @@
+import 'package:task_management/helpers/contants.dart';
+
+extension DateTimeExtension on DateTime {
+  String get date {
+    return "$day/$monthName/$year";
+  }
+
+  String get monthName {
+    return months[month - 1];
+  }
+
+  String get leftTime {
+    final now = DateTime.now();
+    final diff = difference(now);
+    final int days = diff.inDays;
+    final int weeks = diff.inDays ~/ 7;
+    final hours = diff.inHours;
+    if (weeks > 0) {
+      return "$weeks weeks";
+    } else if (days > 0) {
+      return "$days days";
+    } else {
+      return "$hours hours";
+    }
+  }
+}
